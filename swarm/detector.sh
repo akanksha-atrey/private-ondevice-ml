110eabde15100a4d29f91eee8b49e8f938105b06
akanksha-atrey <akanksha.atrey@gmail.com>
2023-04-24 11:39:03 -0400

    swarm script for detection

---
+++ swarm/detector.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+#SBATCH --job-name=detector
+#SBATCH --output=./results/output/res_%j.txt             # output file
+#SBATCH -e ./results/output/res_%j.err                   # File to which STDERR will be written
+#SBATCH --partition=longq                              	   # Partition to submit to
+#
+#SBATCH --ntasks=4
+#SBATCH --time=10-01:00:00
+#SBATCH --mem-per-cpu=8000                                # Memory in MB per cpu allocated
+
+set -x
+
+source /home/aatrey/mypython/bin/activate
+
+echo "SLURM_JOBID: " $SLURM_JOBID
+echo "Running defense detector..."
+
+modeltype=$1
+noisebounds=$2
+numqueries=$3
+
+python3 -m src.defense -model_type $modeltype -noise_bounds $noisebounds -num_queries $numqueries
+
+wait
+
+echo "Done running defense!"
+exit
